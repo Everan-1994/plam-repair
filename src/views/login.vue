81b2519e0b0f678da86b1078dcaad53f72a68faf
Everan <everan@aliyun.com>
2018-04-17 22:22:19 +0800

    update login.vue

--- src/views/login.vue
+++ src/views/login.vue
@@ -14,23 +14,35 @@
                     <Form ref="loginForm" :model="form" :rules="rules">
                         <FormItem prop="userName">
                             <Input v-model="form.userName" placeholder="请输入用户名">
-                                <span slot="prepend">
+                            <span slot="prepend">
                                     <Icon :size="16" type="person"></Icon>
                                 </span>
                             </Input>
                         </FormItem>
                         <FormItem prop="password">
                             <Input type="password" v-model="form.password" placeholder="请输入密码">
-                                <span slot="prepend">
+                            <span slot="prepend">
                                     <Icon :size="14" type="locked"></Icon>
                                 </span>
                             </Input>
                         </FormItem>
+                        <FormItem prop="captcha">
+                            <Input type="text" v-model="form.captcha"
+                                   placeholder="请输入验证码">
+                            <span slot="prepend">
+                                    <Icon :size="14" type="ios-keypad"></Icon>
+                                </span>
+                            <span slot="append" class="img-code">
+                                    <img ref="captcha" src="../images/everan.gif" title="刷新验证码" alt="验证码"
+                                         @click="codeInit">
+                                </span>
+                            </Input>
+                        </FormItem>
                         <FormItem>
-                            <Button @click="handleSubmit" type="primary" long>登录</Button>
+                            <Button @click="handleSubmit" type="primary" long :loading="loading">登录</Button>
                         </FormItem>
                     </Form>
-                    <p class="login-tip">输入任意用户名和密码即可</p>
+                    <p class="login-tip">欢迎使用凯悦科技 -- 掌上报修管理系统</p>
                 </div>
             </Card>
         </div>
@@ -38,44 +50,85 @@
 </template>
 
 <script>
-import Cookies from 'js-cookie';
-export default {
-    data () {
-        return {
-            form: {
-                userName: 'iview_admin',
-                password: ''
-            },
-            rules: {
-                userName: [
-                    { required: true, message: '账号不能为空', trigger: 'blur' }
-                ],
-                password: [
-                    { required: true, message: '密码不能为空', trigger: 'blur' }
-                ]
-            }
-        };
-    },
-    methods: {
-        handleSubmit () {
-            this.$refs.loginForm.validate((valid) => {
-                if (valid) {
-                    Cookies.set('user', this.form.userName);
-                    Cookies.set('password', this.form.password);
-                    this.$store.commit('setAvator', 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3448484253,3685836170&fm=27&gp=0.jpg');
-                    if (this.form.userName === 'iview_admin') {
-                        Cookies.set('access', 0);
-                    } else {
-                        Cookies.set('access', 1);
-                    }
-                    this.$router.push({
-                        name: 'home_index'
-                    });
+    import axios from 'axios';
+//    import JWT from '../helpers/jwt';
+//    import {path} from '../helpers/path';
+//    import Cookies from 'js-cookie';
+
+    export default {
+        data () {
+            return {
+                form: {
+                    userName: '',
+                    password: '',
+                    captcha: ''
+                },
+                captcha_key: 'captcha-no',
+                default_src: '',
+                loading: false,
+                rules: {
+                    userName: [
+                        { required: true, message: '账号不能为空', trigger: 'blur' }
+                    ],
+                    password: [
+                        { required: true, message: '密码不能为空', trigger: 'blur' }
+                    ],
+                    captcha: [
+                        { required: true, message: '验证码不能为空', trigger: 'blur' }
+                    ]
                 }
-            });
+            };
+        },
+        mounted() {
+            this.default_src = this.$refs.captcha.src;
+            // this.codeInit();
+        },
+        methods: {
+            handleSubmit () {
+                let _this = this;
+                _this.loading = true;
+                let formData = {
+                    'username': _this.form.userName,
+                    'password': _this.form.password,
+                    'captcha': _this.form.captcha,
+                    'captcha_key': _this.captcha_key,
+                };
+                this.$refs.loginForm.validate((valid) => {
+                    if (valid) {
+                        axios.post(path + '/api/authorizations', formData).then(response => {
+                            let data = response.data;
+                            JWT.setToken('Bearer ' + data.meta.access_token);
+                            Cookies.set('user', data.data.name);
+                            Cookies.set('password', _this.form.password);
+                            Cookies.set('access', data.data.level);
+                            _this.$store.commit('setAvator', 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3448484253,3685836170&fm=27&gp=0.jpg');
+                            _this.$Message.success({
+                                content: '登陆成功',
+                                onClose: () => {
+                                    _this.$router.push({name: 'home_index'})
+                                }
+                            });
+                        }).catch(error => {
+                            _this.loading = false;
+                            _this.$Message.error(error.response.data.message || '服务器异常');
+                        });
+                    } else {
+                        _this.loading = false;
+                    }
+                });
+            },
+            codeInit() {
+                let _this = this;
+                _this.$refs.captcha.src = _this.default_src;
+                axios.get(path + '/api/captchas/' + _this.captcha_key).then(response => {
+                    _this.captcha_key = response.data.captcha_key;
+                    setTimeout(() => {
+                        _this.$refs.captcha.src = response.data.captcha_image_content;
+                    }, 1000);
+                });
+            }
         }
-    }
-};
+    };
 </script>
 
 <style>
